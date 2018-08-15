a7aaec5305ef672d92fb0be4ee75095dcbe17a99
Manish <manish.vermase@gail.com>
2018-08-15 13:58:47 +0530

    nvbn

--- CakeShop/Controllers/HomeController.cs
+++ CakeShop/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace CakeShop.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
-
+              
             return View();
         }
     }
